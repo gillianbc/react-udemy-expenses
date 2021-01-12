d741c10d2fbb1c58c1a80a9b63af562edf70f755
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2021-01-12 20:16:21 +0000

    404 not found page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
@@ -8,19 +8,21 @@ const ExpenseDashboardPage = () => ( <div>This is the dashboard</div> )
 const AddExpensePage = () => ( <div>This is the add expense page</div> )
 const EditExpensePage = () => ( <div>This is the edit expense page</div> )
 const HelpPage = () => ( <div>This is the help page</div> )
+const NotFoundPage = () => ( <div>Page not found 404</div> )
 
 // The const above holds a function that implicitly returns us a JSX template.
 // We give the function reference to Route.
-
+// There is no path for the NotFoundPage so this will always be a match.  It has to come last otherwise
+// that component would appear on every page as well as the other matching components
 const router = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/create" component={ AddExpensePage }/>
       <Route path="/edit" component={ EditExpensePage }/>
       <Route path="/help" component={ HelpPage }/>
       <Route path="/" component={ ExpenseDashboardPage } exact={true}/>
-
-    </div>
+      <Route component={ NotFoundPage }/>
+    </Switch>
   </BrowserRouter>
 )
 
