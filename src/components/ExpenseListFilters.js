9ef05a2cbd3c6b99d2ced40158b381af68658b40
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2021-02-14 21:30:36 +0000

    Not working

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import {connect} from "react-redux";
-import {setTextFilter, sortByAmount, sortByDate} from "../actions/filters";
+import {setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate} from "../actions/filters";
+import { DateRangePicker } from 'react-dates'
 class ExpenseListFilters extends React.Component {
+    // We don't need a constructor to define the state due to the plugin we're using (babel-plugin-transform-class-properties).
+    // Without the plugin, you have to do it the first way since state must be set up in the constructor.
+    state = {
+        calendarFocused: null
+    }
+    onDatesChange = ({ startDate, endDate }) => {
+        this.props.dispatch(setStartDate({ startDate }))
+        this.props.dispatch(setEndDate({ endDate }))
+    };
+    onFocusChange = ( focusedInput ) => {
+        this.state.calendarFocused = focusedInput
+    };
     render(){
         return (<div>
             <input type="text" value={this.props.filters.text} onChange={ (e) => {
@@ -16,6 +29,17 @@ class ExpenseListFilters extends React.Component {
                     <option value="date">Date</option>
                     <option value="amount">Amount</option>
                 </select>
+                <div>
+                    <DateRangePicker>
+                        startDate={this.props.startDate}
+                        endDate={this.props.endDate}
+                        onDatesChange={ this.onDatesChange }
+                        focusedInput={ this.state.calendarFocused }
+                        onFocusChange={ this.onFocusChange }
+                        startDateId="start"
+                        endDateId="end"
+                    </DateRangePicker>
+                </div>
             </div>
 
         </div>)
