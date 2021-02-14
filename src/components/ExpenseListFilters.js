db50de3a216559caace257e64bf851cb219509ab
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2021-02-14 20:39:58 +0000

    Converted ExpenseListFilters to a class based component (we'll need state to track whether the date pickers should be in focus)

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import {connect} from "react-redux";
 import {setTextFilter, sortByAmount, sortByDate} from "../actions/filters";
-// These are what's known as a 'controlled input'
-// The value is read from the state and we use JS to update the state
-const ExpenseListFilters = ( props ) => (
-    <div>
-        <input type="text" value={props.filters.text} onChange={ (e) => {
-            props.dispatch(setTextFilter(e.target.value))
-        }}/>
-        <div>
-            <select
-                value={props.filters.sortBy}
-                onChange={ (e) => {
-                    e.target.value === "date" ? props.dispatch(sortByDate()) : props.dispatch(sortByAmount())
-                }}>
-                <option value="date">Date</option>
-                <option value="amount">Amount</option>
-            </select>
-        </div>
+class ExpenseListFilters extends React.Component {
+    render(){
+        return (<div>
+            <input type="text" value={this.props.filters.text} onChange={ (e) => {
+                this.props.dispatch(setTextFilter(e.target.value))
+            }}/>
+            <div>
+                <select
+                    value={this.props.filters.sortBy}
+                    onChange={ (e) => {
+                        e.target.value === "date" ? this.props.dispatch(sortByDate()) : this.props.dispatch(sortByAmount())
+                    }}>
+                    <option value="date">Date</option>
+                    <option value="amount">Amount</option>
+                </select>
+            </div>
 
-    </div>
-)
+        </div>)
+    }
+}
 
 // Grab data from the connected state and put it in the props
 const mapStateToProps = (state) => {
