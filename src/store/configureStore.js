48db297f078df9af9e905a0ea1aab0b468a702a2
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2021-02-14 13:00:29 +0000

    Added redux dev-tools - note enabled in my personal chrome profile only

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,7 +6,8 @@ const store = createStore(
     combineReducers({
         expenses: expensesReducer,
         filters: filtersReducer
-    })
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 export default store
