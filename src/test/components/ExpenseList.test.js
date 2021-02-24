eb5969938ca8bf45a2390266d37f5b21cc766096
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2021-02-24 21:40:39 +0000

    Multiple snapshots from the same test suite - test for an empty array of expenses

--- src/test/components/ExpenseList.test.js
+++ src/test/components/ExpenseList.test.js
@@ -6,8 +6,15 @@ import testExpenseData from '../resources/expenses'
 * Tests the snapshot of the ExpenseList component
 * */
 
-it('The snapshot should match', () => {
+it('The snapshot should match when there is an array of expenses', () => {
     // Remember, these curlies are not object brackets, they're JSX brackets
     const wrapper = shallow(<ExpenseList expenses={ testExpenseData }/>);
     expect(wrapper).toMatchSnapshot();
 })
+
+it('The snapshot should match when there is an empty array of expenses', () => {
+    // Remember, these curlies are not object brackets, they're JSX brackets
+    const wrapper = shallow(<ExpenseList expenses={ [] }/>);
+    expect(wrapper).toMatchSnapshot();
+})
+
