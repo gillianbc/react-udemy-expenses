411817459d7e6ccc22f6a4482f3ee3e64a0cb366
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2021-03-04 21:57:28 +0000

    ExpenseForm test for sample expense working

--- src/test/components/ExpenseForm.test.js
+++ src/test/components/ExpenseForm.test.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import ExpenseForm from "../../components/ExpenseForm";
+import sampleExpenses from '../resources/expenses'
 
 test('should render expense form with default values if we pass it no expense', () => {
-    // jest.mock('moment')
     const wrapper = shallow(<ExpenseForm/>)
+    expect(wrapper).toMatchSnapshot();
+})
+
+test('should render expense form with default values if we pass it all the fields from a sample expense', () => {
+    const wrapper = shallow(<ExpenseForm expense={sampleExpenses[0]}/>)
     expect(wrapper).toMatchSnapshot();
 })
